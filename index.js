5a472f6b082dc7055c1c02f6256c0ae3faba7920
Sloian <andriysolonynka10@gmail.com>
2022-09-10 16:55:09 +0300

    jsonplaceholderCompleted

--- index.js
+++ index.js
@@ -1,45 +1,67 @@
 const postList = document.querySelector('.posts-list');
 const getPostsBtn = document.querySelector('.get-posts');
+const getAuthorBtn = document.querySelector('.get-author');
 
-const limitPost = 8;
+const limitPost = 80;
 
 const state = {
   posts: [],
-  somePost: {}
+  usersId:[]
 };
 
+const getSomeBody = (postId) => {
 
-const getSomeBody = (index) => {
-  const clickPost = state.posts[index];
-  console.log(clickPost)
-  state.somePost = clickPost;
+  if(postList.classList.contains( "scroll-style" )){
+    postList.classList.remove("scroll-style")
+  }
 
-  fillPostsList(state.posts);
-  postList.innerHTML = "";
+  const post = state.posts.find(item => item.id === postId)
+  console.log(postList.style);
 
-  const somePostHTML =  `
-  <div class="wrapper">
-
-
-    <div class="somePost-wrapper">
-      <h1 class="somePost-title">${state.somePost.title}</h1>
-      <p class="somePost-body">${state.somePost.body}</p>
-      <p class="somePost-userId">UserId: ${state.somePost.userId}</p>
-    </div>
-  </div>
-  `;
-
-  postList.innerHTML = somePostHTML;
+  postList.innerHTML = somePostHTML(post);
 };
 
-
-const createPost = (post, index) => `
+const getHeaderPostList = (post) =>  `
   <div class="post">
 
     <div class="post-wrapper">
       <ol class="ball">
         <li>
-          <h1 class="post-title" onclick="getSomeBody(${index})">${post.title}</h1>
+          <h1 class="post-title" onclick="getSomeBody(${post.id})">${post.title}</h1>
+        </li>
+      </ol>
+    </div>
+
+  </div>
+  `;
+
+
+const getUserPosts = (userId) =>{
+  const userPosts = state.posts.filter(element => element.userId === userId)
+  console.log(userPosts)
+  postList.innerHTML = "";
+  userPosts.forEach(element => postList.innerHTML += getHeaderPostList(element));
+}
+
+const somePostHTML = (post) =>  `
+  <div class="wrapper">
+
+    <div class="somePost-wrapper">
+      <h1 class="somePost-title">${post.title}</h1>
+      <p class="somePost-body">${post.body}</p>
+      <p class="somePost-userId">UserId: ${post.userId}</p>
+    </div>
+  </div>
+  `;
+
+
+const createPost = (post) => `
+  <div class="post">
+
+    <div class="post-wrapper">
+      <ol class="ball">
+        <li>
+          <h1 class="post-title" onclick="getSomeBody(${post.id})">${post.title}</h1>
         </li>
       </ol>
     </div>
@@ -48,31 +70,99 @@ const createPost = (post, index) => `
 
 `;
 
+const createAuthor = (post) =>`
+  <div class="post">
+
+    <div class="post-wrapper">
+      <ol class="ball">
+        <li>
+          <h1 class="post-title" onclick="getUserPosts(${post})">userId: ${post}</h1>
+        </li>
+      </ol>
+    </div>
+
+  </div>
+`
+
+
+
+const fillAuthorList = (posts) =>{
+
+  postList.innerHTML = "";
+  if (posts.length){
+    posts.filter(item =>{
+      if(!state.usersId.length){
+        state.usersId.push(item.userId)
+      }
+      else if(!state.usersId.includes(item.userId)){
+        state.usersId.push(item.userId)
+      }
+    })
+
+    state.usersId.forEach(element => postList.innerHTML += createAuthor(element));
+
+    console.log(posts)
+  }
+
+
+}
 
 const fillPostsList = (posts) =>{
-
   postList.innerHTML = "";
 
   if (posts.length){
-    posts.forEach((post, index) => postList.innerHTML += createPost(post,index) )
+    posts.forEach((post) => postList.innerHTML += createPost(post) )
   }
 };
 
-getPostsBtn.addEventListener('click', async () =>{
-
+getAuthorBtn.addEventListener('click', async() =>{
   if(state.posts.length === 0){
     await getPostsRequest();
   }
 
-  console.log(state.posts.length);
+  if(getPostsBtn.classList.contains( "button-active" )){
+    getPostsBtn.classList.remove("button-active")
+  }
+  getAuthorBtn.classList.add("button-active")
 
+  if(postList.classList.contains( "scroll-style" )){
+    postList.classList.remove("scroll-style")
+  }
+
+
+
+
+  console.log(state.posts.length);
+  fillAuthorList(state.posts);
+  console.log(state)
+})
+
+
+getPostsBtn.addEventListener('click', async () =>{
+  if(state.posts.length === 0){
+    await getPostsRequest();
+  }
+
+  if(getAuthorBtn.classList.contains( "button-active" )){
+    getAuthorBtn.classList.remove("button-active")
+  }
+
+  getPostsBtn.classList.add("button-active")
+  postList.classList.add("scroll-style")
+
+
+  console.log(state.posts.length);
   fillPostsList(state.posts);
   console.log(state)
 })
+
+
 
 const getPostsRequest = () =>{
   return fetch('https://jsonplaceholder.typicode.com/posts?_limit=' + limitPost)
   .then((res) => res.json())
   .then((posts) => state.posts = state.posts.concat(posts))
 };
+
+
 
